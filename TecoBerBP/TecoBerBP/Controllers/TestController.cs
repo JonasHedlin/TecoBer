b0bc35d0bf74a2353ebf84f394a2e3d8f813dd40
Jonas Hedlin <jonas.hedlin@telia.com>
2017-03-26 17:30:22 +0200

    First Test

---
+++ TecoBerBP/TecoBerBP/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TecoBerBP.Models.Test;
+
+namespace TecoBerBP.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var IndexVM = new IndexVM();
+            return View(IndexVM);
+        }
+    }
+}
