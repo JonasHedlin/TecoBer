e8440cdc21e13c6516412999aefc9572d7596bb4
Jonas Hedlin <jonas.hedlin@telia.com>
2017-04-17 12:46:02 +0200

    Added register and log in

---
+++ TecoBerBP/TecoBerBP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TecoBerBP.Startup))]
+namespace TecoBerBP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
