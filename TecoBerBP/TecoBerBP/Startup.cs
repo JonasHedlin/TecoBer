0b9e985d9dd34102730dcb8d448dabcc41321de3
Jonas Hedlin <jonas.hedlin@telia.com>
2017-04-24 09:18:33 +0200

    Changed to Office 365 sign in procedure

---
+++ TecoBerBP/TecoBerBP/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace TecoBerBP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
