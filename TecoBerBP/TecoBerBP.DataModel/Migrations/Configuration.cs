ddad061d83785354b280e06325f17f70f6710f05
Jonas Hedlin <jonas.hedlin@berotec.se>
2017-07-28 12:44:33 +0200

    Updated seeding-method

--- TecoBerBP/TecoBerBP.DataModel/Migrations/Configuration.cs
+++ TecoBerBP/TecoBerBP.DataModel/Migrations/Configuration.cs
@@ -40,42 +40,69 @@ namespace TecoBerBP.DataModel.Migrations
                   new DataClasses.BPRole { Name = "Användare", AuthenticationLevel = 1 }
                 );
 
-                // Import data (user data) from excel sheet.
-                ImportExcelDataClass IEDC = new ImportExcelDataClass();
 
-                List<BerotecUserClass> BUsers = IEDC.GetExcelData();
-
-                foreach (BerotecUserClass BUser in BUsers)
-                {
-                    context.BPUsers.AddOrUpdate(
-                      u => u.CLSID,
-                      new DataClasses.BPUser
-                      {
-                          Name = BUser.Name,
-                          SurName = BUser.SurName,
-                          CLSID = string.IsNullOrEmpty(BUser.CLSID) == true ? Guid.NewGuid().ToString() : BUser.CLSID, // TODO! Must change so that we have this field in the excel-sheet to and only generate a new Guid if that field is empty!
-                          Gender = (EnGender)(string.IsNullOrEmpty(BUser.Gender) == false ? (BUser.Gender == "M" ? 1 : (BUser.Gender == "K" ? 0 : 10)) : 10), // a ? b : (c ? d : e)
-                          Email = BUser.Email,
-                          AltEmail = BUser.AltEmail,
-                          Titel = BUser.Titel,
-                          AreaOfExpertise = BUser.AreaOfExpertise,
-                          Cell = string.IsNullOrEmpty(BUser.Cell) == false ? ((BUser.Cell).Replace("-", "")).Replace(" ", "") : "", // No space ' ' or '-' chars in phone number.
-                          Company = BUser.Company,
-                          CompanyNo = BUser.CompanyNo,
-                          CompanyAddress = BUser.CompanyAddress,
-                          CompanyZip = BUser.CompanyZip,
-                          CompanyCity = BUser.CompanyCity,
-                          OfficeLocation = BUser.OfficeLocation,
-                          CompanyLead = BUser.CompanyLead,
-                          DateOfBirth = BUser.DateOfBirth,
-                          JoinedDate = BUser.JoinedDate,
-                          QuitDate = BUser.QuitDate,
-                          Comment = BUser.Comment,
-                          Status = (EnStatus)(BUser.Status == "Aktiv" ? 1 : (BUser.Status == "Slutat" ? 0 : 10)),
-                          RoleId = EnAuthenticationLevel.User // (1) Always start as user (standard user).
-                      }
+                context.BPActivities.AddOrUpdate(
+                    ua => ua.ActivityId,
+                    new DataClasses.BPActivity { Name = "Findings - hos kun", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Arbetsmarknadsdag - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Arbetsmarknadsdag - Hjälpa till", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Be Orange nivaå 2-6 - Deltaga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Be Orange Refresh - Deltaga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Berotec Säljutbildning - Deltaga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Berotecträff - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Blogga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Extern kurs - Deltaga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Etern utbildning med Berotec-koppling - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Externa seminarium med Berotec-koppling - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Förmedling av personlig kontakt", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Intern utbildning - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Interna seminarium - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Kompetensgrupp - Äga/Driva", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Konferansresa anordna i Berotecs regi - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Konferansresa anordnad i Berotecs regi - Deltaga", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Kundcase - Skapa", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Kundevent - Anordna", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 },
+                    new DataClasses.BPActivity { Name = "Kundmöte - Deltaga med unik kompetens", Duration = 12, DurationUnit = EnDurationUnit.Month, Point = 1 }
                     );
-                }
+
+
+                // Import data (user data) from excel sheet.
+                //ImportExcelDataClass IEDC = new ImportExcelDataClass();
+
+                //List<BerotecUserClass> BUsers = IEDC.GetExcelData();
+
+                //foreach (BerotecUserClass BUser in BUsers)
+                //{
+                //    context.BPUsers.AddOrUpdate(
+                //      u => u.CLSID,
+                //      new DataClasses.BPUser
+                //      {
+                //          Name = BUser.Name,
+                //          SurName = BUser.SurName,
+                //          CLSID = string.IsNullOrEmpty(BUser.CLSID) == true ? Guid.NewGuid().ToString() : BUser.CLSID, // TODO! Must change so that we have this field in the excel-sheet to and only generate a new Guid if that field is empty!
+                //          Gender = (EnGender)(string.IsNullOrEmpty(BUser.Gender) == false ? (BUser.Gender == "M" ? 1 : (BUser.Gender == "K" ? 0 : 10)) : 10), // a ? b : (c ? d : e)
+                //          Email = BUser.Email,
+                //          AltEmail = BUser.AltEmail,
+                //          Titel = BUser.Titel,
+                //          AreaOfExpertise = BUser.AreaOfExpertise,
+                //          Cell = string.IsNullOrEmpty(BUser.Cell) == false ? ((BUser.Cell).Replace("-", "")).Replace(" ", "") : "", // No space ' ' or '-' chars in phone number.
+                //          Company = BUser.Company,
+                //          CompanyNo = BUser.CompanyNo,
+                //          CompanyAddress = BUser.CompanyAddress,
+                //          CompanyZip = BUser.CompanyZip,
+                //          CompanyCity = BUser.CompanyCity,
+                //          OfficeLocation = BUser.OfficeLocation,
+                //          CompanyLead = BUser.CompanyLead,
+                //          DateOfBirth = BUser.DateOfBirth,
+                //          JoinedDate = BUser.JoinedDate,
+                //          QuitDate = BUser.QuitDate,
+                //          Comment = BUser.Comment,
+                //          Status = (EnStatus)(BUser.Status == "Aktiv" ? 1 : (BUser.Status == "Slutat" ? 0 : 10)),
+                //          RoleId = EnAuthenticationLevel.User // (1) Always start as user (standard user).
+                //      }
+                //    );
+                //}
+
                 context.SaveChanges();
             }
             catch (DbEntityValidationException e)
